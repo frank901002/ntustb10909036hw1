b22e3671fc71c9ed9f6956c3e69839abfd566d70
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-10-06 15:43:12 +0800

    second commit

--- firstApp.py
+++ firstApp.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run()  
-
 @app.route("/readme")
 def readme():
     return "This is ream me page. Please read me first!!"    
